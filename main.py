eae41a1ccce76a0d147e46650f4d0c4598aaa606
roshanroxx <82316084+roshanroxx@users.noreply.github.com>
2021-08-30 12:47:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title("clock")
+
+
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    Label.config(text=string)
+    Label.after(1000, time)
+
+
+Label = Label(root, font=("comic", 30), background="black", foreground="cyan")
+Label.pack(anchor="center")
+time()
+
+
+mainloop()
